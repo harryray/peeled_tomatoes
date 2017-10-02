d361cad73b3f700cfef50d8963860a79dbf1afd7
Harry Ray <harryraywebdev@gmail.com>
2017-10-02 09:49:13 +0000

    Push to Heroku

--- app/controllers/build_a_duck_controller.rb
+++ app/controllers/build_a_duck_controller.rb
@@ -1,3 +1,10 @@
 class BuildADuckController < ApplicationController
-  
+  def calculate_quote
+    size = params[:quote][:size]
+    material = params[:quote]["material"]
+    colour =params[:quote]["colour"]
+    accessories = params[:quote]["accessories"]
+    quote = ["size": size,"material": material,"colour": colour,"accessories": accessories]
+    render json: quote
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  # Build a duck project
   resources :build_a_duck
+  match "/build_a_duck/calculate" => "build_a_duck#calculate_quote", via: :post
+  
+  # Project List Project
   resources :projects
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "application#index"
   
