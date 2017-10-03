db18029522ca25eaa63480f8580ca63d63cfb9f5
Harry Ray <harryraywebdev@gmail.com>
2017-10-03 07:58:56 +0000

    Update scope of price variable.

--- app/controllers/build_a_duck_controller.rb
+++ app/controllers/build_a_duck_controller.rb
@@ -1,10 +1,34 @@
 class BuildADuckController < ApplicationController
   def calculate_quote
-    size = params[:quote][:size]
-    material = params[:quote]["material"]
-    colour =params[:quote]["colour"]
-    accessories = params[:quote]["accessories"]
-    quote = ["size": size,"material": material,"colour": colour,"accessories": accessories]
+    # Deal with params
+    quote = params[:quote]
+    quote = JSON.parse quote
+    size = quote["size"]
+    material = quote["material"]
+    colour = quote["colour"]
+    accessories_hat = quote["accessories_hat"]
+    accessories_eyewear = quote["accessories_eyewear"]
+    accessories_facial_hair = quote["accessories_facial_hair"]
+    accessories = "#{accessories_hat}, #{accessories_eyewear}, #{accessories_facial_hair}"
+    
+    # Calculate price
+    @price = 5.00
+    if "material" == "wood" then
+      @price = @price*2.50
+    end
+    if "material" == "plastic" then
+      @price = @price*0.80
+    end
+    if "size" == "medium" then
+     @price = @price*1.25
+    end
+    if "size" == "large" then
+     @price = @price*1.50
+    end
+    
+    quote = ["size": size,"material": material,"colour": colour,"accessories": accessories,"price": @price]
+    
+    
     render json: quote
   end
 end
