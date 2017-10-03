a1d0fb47aadfb247bda9c7c8a39c2f144597a42d
Harry Ray <harryraywebdev@gmail.com>
2017-10-03 08:15:44 +0000

    Update calculate_quote method.

--- app/controllers/build_a_duck_controller.rb
+++ app/controllers/build_a_duck_controller.rb
@@ -12,24 +12,18 @@ class BuildADuckController < ApplicationController
     accessories = "#{accessories_hat}, #{accessories_eyewear}, #{accessories_facial_hair}"
     
     # Calculate price
-    @base_price = 5.00
-    if "material" == "rubber" then
-      @price = @base_price*1
+    @price = 5.00
+    if material == "wood" then
+      @price = @price*2.50
     end
-    if "material" == "wood" then
-      @price = @base_price*2.50
+    if material == "plastic" then
+      @price = @price*0.80
     end
-    if "material" == "plastic" then
-      @price = @base_price*0.80
+    if size == "medium" then
+     @price = @price*1.25
     end
-    if "size" == "small" then
-     @price = @base_price*1
-    end
-    if "size" == "medium" then
-     @price = @base_price*1.25
-    end
-    if "size" == "large" then
-     @price = @base_price*1.50
+    if size == "large" then
+     @price = @price*1.50
     end
     
     quote = ["size": size,"material": material,"colour": colour,"accessories": accessories,"price": @price]
